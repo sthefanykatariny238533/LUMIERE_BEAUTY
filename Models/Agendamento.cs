fb5681029c976fe9a41fb0be2e11ad11bdae92f2
Guides Felipe <guidesmiranda1@gmail.com>
2026-09-14 10:47:26 -0400

    Criação da Pasta Models + Tabela Agendamento

---
+++ Models/Agendamento.cs
@@ -0,0 +1,19 @@
+﻿namespace Lumiere_Beauty.Models
+{
+    public class Agendamento
+    {
+        public int id_agend { get; set; }
+
+        public  DateTime data_agend { get; set; } 
+
+        public TimeOnly horario_agend { get; set; }
+
+        public string status_agend { get; set; } 
+
+        public int id_servico_fk { get; set; }
+
+        public int id_profissional_fk { get; set; }
+
+        public int id_cliente_fk { get; set; }
+    }
+}
