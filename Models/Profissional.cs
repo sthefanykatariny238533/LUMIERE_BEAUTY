58b2a671fcea0a914b863656e57165eb99ea792a
Guides Felipe <guidesmiranda1@gmail.com>
2026-09-21 09:31:13 -0400

    Update Profissional.cs

--- Models/Profissional.cs
+++ Models/Profissional.cs
@@ -2,12 +2,12 @@
 {
     public class Profissional
     {
-        public int id_profissional { get; set; }
+        public int id { get; set; }
 
-        public string nome_profi { get; set; }
+        public string nome { get; set; }
 
-        public int telefone_profi { get; set; }
+        public int telefone { get; set; }
 
-        public string especialidade_profi { get; set; }
+        public string especialidade { get; set; }
     }
 }
