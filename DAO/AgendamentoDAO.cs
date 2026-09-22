b35ee58a1e8f915cabf230f3f6d8519d680e42e5
Guides Felipe <guidesmiranda1@gmail.com>
2026-09-22 10:59:26 -0400

    Criação DAO Agendamento

--- DAO/AgendamentoDAO.cs
+++ DAO/AgendamentoDAO.cs
@@ -8,57 +8,57 @@ namespace Lumiere_Beauty.DAO
     {
         private readonly Conexao _conexao;
 
-        // A Conexao é injetada pelo container de dependências
-
         public AgendamentoDAO(Conexao conexao)
         {
             _conexao = conexao;
         }
 
-
-        // READ - lista todos os processos
         public List<Agendamento> Listar()
         {
-            var lista = new List<Agendamento>();
-
-            var comando = _conexao.CreateCommand("SELECT * FROM agendamento;");
-
-            var leitor = (MySqlDataReader)comando.ExecuteReader();
-
-            while (leitor.Read())
+            try
             {
-                lista.Add(MapearAgendamento(leitor));
+                var lista = new List<Agendamento>();
+
+                using var con = _conexao.GetConnection();
+
+                string sql = @"
+                SELECT 
+                    id_agend,
+                    data_agend,
+                    horario_agend,
+                    status_agend,
+                    id_cliente_fk,
+                    id_profissional_fk,
+                    id_servico_fk
+                FROM Agendamento;
+            ";
+
+                using var comando = con.CreateCommand();
+                comando.CommandText = sql;
+
+                using var leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    var agendamento = new Agendamento();
+
+                    agendamento.id = leitor.GetInt32("id_agend");
+                    agendamento.data = leitor.GetDateTime("data_agend");
+                    agendamento.horario = TimeOnly.FromTimeSpan(leitor.GetTimeSpan("horario_agend"));
+                    agendamento.status = leitor.GetString("status_agend");
+                    agendamento.id_cliente_fk = leitor.GetInt32("id_cliente_fk");
+                    agendamento.id_profissional_fk = leitor.GetInt32("id_profissional_fk");
+                    agendamento.id_servico_fk = leitor.GetInt32("id_servico_fk");
+
+                    lista.Add(agendamento);
+                }
+
+                return lista;
             }
-            return lista;
-        }
-
-
-        // Método auxiliar: converte a linha atual do leitor em um objeto Processo
-        // Usa o DAOHelper para ler com segurança as colunas que podem ser NULL
-        private static Agendamento MapearAgendamento(MySqlDataReader leitor)
-        {
-            return new Agendamento
+            catch
             {
-                id = leitor.GetInt32("id"),
-
-                data = leitor.GetDateTime("data"),
-
-                horario = TimeOnly.FromTimeSpan(leitor.GetTimeSpan("horario")),
-
-                status = leitor.GetString("status"),
-
-                id_servico_fk = leitor.GetInt32("id_servico_fk"),
-
-                id_profissional_fk = leitor.GetInt32("id_profissional_fk"),
-
-                id_cliente_fk = leitor.GetInt32("id_cliente_fk")
-
-            };
+                throw;
+            }
         }
-
-
-
-
-
     }
 }
